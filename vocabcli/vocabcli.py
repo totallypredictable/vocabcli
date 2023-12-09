50fdea586416bd5dd90e8780ba5b53a9cb515ab8
Ahmet Caliskan <root@systemalert.org>
2023-12-09 22:14:20 +0100

    adds add functionality

--- vocabcli/vocabcli.py
+++ vocabcli/vocabcli.py
@@ -2,8 +2,9 @@
 # vocabcli/vocabcli.py
 
 from pathlib import Path
-from typing import Any, Dict, NamedTuple
+from typing import Any, Dict, NamedTuple, List
 
+from vocabcli import DB_READ_ERROR
 from vocabcli.database import DatabaseHandler
 
 
@@ -15,3 +16,26 @@ class CurrentWord(NamedTuple):
 class Worder:
     def __init__(self, db_path: Path) -> None:
         self._db_handler = DatabaseHandler(db_path)
+
+    def add(self, name: str, article: str|None = None, gender: str|None = None, typ: str|None = None) -> CurrentWord:
+        """Add a new word to the database."""
+        if article == None:
+            article = NA
+        if gender == None:
+            gender == NA
+        if typ == None:
+            typ == NA
+        word = {
+                "name": name,
+                "article": article,
+                "gender": gender,
+                "typ": typ,
+        }
+        read = self._db_handler.read_words()
+        if read.error == DB_READ_ERROR:
+            return CurrentWord(word, read.error)
+        read.word_list.append(word)
+        write = self._db_handler.write_words(read.word_list)
+        return CurrentWord(word, write.error)
+
+
