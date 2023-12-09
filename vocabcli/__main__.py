d33ea0333cc622ed9ac0768c6c5c6bf8f8e9f758
Ahmet Caliskan <root@systemalert.org>
2023-12-09 01:46:00 +0100

    __main__.py initial

---
+++ vocabcli/__main__.py
@@ -0,0 +1,10 @@
+""" vocabcli entry point script."""
+# vocabcli/__main__.py
+
+from vocabcli import cli, __app_name__
+
+def main():
+    cli.app(prog_name=__app_name__)
+
+if __name__ == "__main__":
+    main()
