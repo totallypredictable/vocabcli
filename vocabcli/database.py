2cbac5f897d4c7b44a1234be3106381ec3c53f12
Ahmet Caliskan <root@systemalert.org>
2023-12-09 19:31:22 +0100

    database.py initial

---
+++ vocabcli/database.py
@@ -0,0 +1,24 @@
+"""This module provides the vocabcli database functionality."""
+# vocabcli/database.py
+
+import configparser
+from pathlib import Path
+
+from vocabcli import DB_WRITE_ERROR, SUCCESS
+
+DEFAULT_DB_FILE_PATH = Path.home().joinpath(
+        "." + Path.home().stem + "_vocab.json"
+)
+
+def get_database_path(config_file: Path) -> Path:
+    """Return the current path to the to-do database."""
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+    return Path(config_parser["General"]["database"])
+
+def init_database(db_path: Path) -> int:
+    """Create the vocabulary database."""
+    try:
+        db_path.write_text("{}")  # Empty vocab dict
+    except OSError:
+        return DB_WRITE_ERROR
