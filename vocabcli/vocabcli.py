cbafad5f1579f11ecff1afd62589233bd84bbd42
Ahmet Caliskan <root@systemalert.org>
2023-12-09 21:30:09 +0100

    initialise Worder class

--- vocabcli/vocabcli.py
+++ vocabcli/vocabcli.py
@@ -1,8 +1,17 @@
 """This module provides the vocabcli model-controller."""
 # vocabcli/vocabcli.py
 
+from pathlib import Path
 from typing import Any, Dict, NamedTuple
+
+from vocabcli.databaswe import DatabaseHandler
+
 
 class CurrentWord(NamedTuple):
     word: Dict[str, Any]
     error: int
+
+
+class Worder:
+    def __init__(self, db_path: Path) -> None:
+        self._db_handler = DatabaseHandler(db_path)
