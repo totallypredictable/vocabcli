24e4f0266ba671259563e47d2674505d6e0434c6
Ahmet Caliskan <root@systemalert.org>
2023-12-09 19:37:40 +0100

    adds init functionality to cli.py

--- vocabcli/cli.py
+++ vocabcli/cli.py
@@ -1,13 +1,40 @@
 """ This module provides vocabcli CLI."""
 # vocabcli/cli.py
 
+from pathlib import Path
 from typing import Optional
 
 import typer
 
-from vocabcli import __app_name__, __version__
+from vocabcli import ERRORS, __app_name__, __version__, config, database
 
 app = typer.Typer()
+
+@app.command()
+def init(
+        db_path: str = typer.Option(
+            str(database.DEFAULT_DB_FILE_PATH),
+            "--db-path",
+            "-db",
+            prompt="vocab database location?",
+        ),
+) -> None:
+    """Initialise the vocab database."""
+    if app_init_error:
+        typer.secho(
+                f'Creating config file failed with "{ERRORS[app_init_error]}"',
+                fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    db_init_error = database.init_database(Path(db_path))
+    if db_init_error:
+        typer.secho(
+                f'Creating database failed with "{ERRORS[db_init_error]}"',
+                fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(f"The vocab database is {db_path}", fg=typer.colors.GREEN)
 
 def _version_callback(value: bool) -> None:
     if value:
